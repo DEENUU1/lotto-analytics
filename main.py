2816275a45f0d5718e0856f5cb7ceac862ec4906
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-04-04 16:54:59 +0200

    fature: create Jupyter Notebook, load data and create function to count winining by year

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from scraper.merge_csv import merge_csv
 
 
 def main() -> None:
-    # merge_csv("./scraped_data/lottoplus", output_filename="merged_lottoplus.csv")
-    # merge_csv("./scraped_data/lotto", output_filename="merged_lotto.csv")
+    merge_csv("./scraped_data/lottoplus", output_filename="merged_lottoplus.csv")
+    merge_csv("./scraped_data/lotto", output_filename="merged_lotto.csv")
     merge_csv("./scraped_data/mini-lotto", output_filename="mini_lotto.csv")
 
 
